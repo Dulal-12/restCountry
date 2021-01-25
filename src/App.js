743236a5604578ddfab833afdefdf36594ad35d2
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-01-25 20:15:16 +0600

    Done my rest country

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import Country from './components/Country/Country';
 function App() {
 
    const [countries , setCountries] = useState([]);
+   const [population , setPopulation] = useState([]);
    useEffect(()=>{
                           fetch('https://restcountries.eu/rest/v2/all')
                           .then(res => res.json())
                           .then(data=> setCountries(data))
    }, [])
-   console.log(countries);
+   //console.log(countries);
+
+   const handleAddPopulation = (country)=>   {
+     //console.log(country);
+     const newPopulation = [...population , country];
+      setPopulation(newPopulation);
+    };
+
+   const totalPopulation = population.reduce(( ential , country )=>ential + country.population , 0);
+
+   
   return (
     <div className="App">
       <h1>Country : {countries.length}</h1>
+     <p>Selected Country Population : {totalPopulation} </p>
       
         {
-          countries.map(cnt => <Country name={cnt.name}></Country>)
+          countries.map(cnt => <Country country = {cnt} key = {cnt.alpha3Code} handleAddPopulation = {handleAddPopulation}></Country>)
         }
      
     </div>
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Country = ({name}) => {
+const Country = ({country , handleAddPopulation}) => {
+
+    const {name , flag , population , region} = country;
+    const countryStyle = {border : '1px solid green' , margin  : '30px'}
+
     return (
-        <div>
-            <h4>{name}</h4>
+        <div style = {countryStyle}>
+
+                        <h4>{name}</h4>
+                        <img src={flag} style={{width : '20px' , height : '20px'}} alt=""/>
+                        <p><small>Population : {population}</small></p>
+                        <p><small>Region : {region}</small></p>
+                        <button onClick = {()=> handleAddPopulation(country)}>Add population</button>
+
         </div>
     );
 };
